934bcdcc642bc34d81cb09db268b98c61995f15b
BruceWeng <weng8916@gmail.com>
2015-06-09 21:09:54 +0800

    Finish obj1

---
+++ obj1.rb
@@ -0,0 +1,25 @@
+class Mycar
+  attr_accessor :year, :color, :model, :current_speed
+  def initialize (y, c, m, s)
+    @year = y
+    @color = c
+    @model = m
+    @current_speed = s
+  end
+
+  def speed_up(number)
+    @current_speed += number
+  end
+
+  def brake(number)
+    @current_speed -= number
+  end
+
+  def shut_down(number)
+    @current_speed = 0
+  end
+end
+  toyota = Mycar.new(2014, "white", "Camry", 0)
+  puts toyota.year
+  puts toyota.color
+  puts toyota.model
