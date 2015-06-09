648ce2037da65d6571a096af14b00d46faf66b1d
BruceWeng <weng8916@gmail.com>
2015-06-09 21:29:24 +0800

    Finish obj_part2_2

---
+++ obj_part2_2.rb
@@ -0,0 +1,43 @@
+class Mycar
+  attr_accessor :year, :color, :model, :current_speed, :mileage
+  def initialize (y, c, m, s, ml)
+    @year = y
+    @color = c
+    @model = m
+    @current_speed = s
+    @mileage = ml
+  end
+
+  def speed_up(number)
+    @current_speed += number
+  end
+
+  def brake(number)
+    @current_speed -= number
+  end
+
+  def shut_down(number)
+    @current_speed = 0
+  end
+
+  def spray_paint(color)
+    self.color = color
+  end
+
+  def gas_mileage(mile)
+    self.mileage += mile
+  end
+
+  def to_s
+    "My car is a #{self.color}, #{self.year}, #{self.model}"
+  end
+end
+toyota = Mycar.new(2014, "white", "Camry", 0, 1000)
+puts toyota.year
+puts toyota.color
+puts toyota.model
+toyota.color = "black"
+puts toyota.color
+puts toyota.spray_paint('red')
+puts toyota.gas_mileage(2000)
+puts toyota
