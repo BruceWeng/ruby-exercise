28a2da47a31fd1419a49ee4078c837d533b5caf1
BruceWeng <weng8916@gmail.com>
2015-06-10 17:48:09 +0800

    Finish inher2

---
+++ inher2.rb
@@ -0,0 +1,23 @@
+class Vehicle
+  @@number_of_vehicles = 0
+
+  def initialize
+    @@number_of_vehicles += 1
+  end
+
+  def self.gas_mileage(miles)
+    puts "Your mileage is #{miles}."
+  end
+end
+
+class MyCar < Vehicle
+  weight = 1500
+end
+
+class MyTruck < Vehicle
+  weight = 2500
+end
+
+Toyota = MyCar.new
+p Toyota.weight
+p Toyota.gas_mileage
